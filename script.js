0e9a1789834516f2eec5676f988acdd55417370c
Navkirat Singh <133486386+nav3431@users.noreply.github.com>
2024-01-23 11:05:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ submitBtn.addEventListener("click", async (e) => {
     e.preventDefault()
     console.log("Clicked!")
     resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
-    let key = "YOUR-API-KEY"
+    let key = "ema_live_tbkiztZDLQApzQ2dweuhtAOtwf34iH4vBt6axEtq"
     let email = document.getElementById("username").value
     let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
     let res = await fetch(url)
